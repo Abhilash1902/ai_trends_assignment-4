b6e3c16af2b73ef7d4be1d1d63e31dc5693da8fb
Abhilash <108549519+Abhilash1902@users.noreply.github.com>
2022-07-24 14:42:46 +0530

    2 python Files added

---
+++ Program_2.py
@@ -0,0 +1,10 @@
+num1=2
+num2=3
+num3=5
+add=num1+num2+num3
+multi=num1*num2*num3
+Div1=num1/num2
+Div2=Div1/num3
+print("Addition of NUmbers is: ",add)
+print("Multiplication of NUmbers is: ",multi)
+print("Division of NUmbers is: ",Div2)
